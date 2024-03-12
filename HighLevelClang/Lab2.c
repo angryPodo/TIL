f5fbf02a669dfd511f8d90f358c31338b4712a26
angrypodo <hanminjae131@gmail.com>
2024-03-12 09:49:16 +0900

    평형계산

---
+++ HighLevelClang/Lab2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    const double koPyeong = 3.3;
+    int input;
+    double result;
+
+    printf("평을 입력하세요: ");
+    scanf("%d",&input);
+
+    result = input*koPyeong;
+    printf("%lf 평방미터입니다.",result);
+
+    return 0;
+}
