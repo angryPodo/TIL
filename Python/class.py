4c9fb748adc1b317181b1dc67c4f5017d79c721e
angrypodo <hanminjae131@gmail.com>
2024-03-12 13:32:19 +0900

    name changed

--- Python/class.py
+++ Python/class.py
@@ -7,8 +7,8 @@ class human():
         human.populality+=1
         
 class sport_player(human):
-    def __init__(self, name2, age2, sex2,speed,power):
-        super().__init__(name2, age2, sex2)
+    def __init__(self, name, age, gender,speed,power):
+        super().__init__(name, age, gender)
         self.speed = speed
         self.power = power
     def move(self):
