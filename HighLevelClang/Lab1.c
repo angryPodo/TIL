71c6407a2f932ab5d5004ea1443d5d8beefd5d59
angrypodo <hanminjae131@gmail.com>
2024-03-12 13:13:50 +0900

    수정

--- HighLevelClang/Lab1.c
+++ HighLevelClang/Lab1.c
@@ -1,15 +1,15 @@
 #include <stdio.h> 
 int main()
 {
-    double light_speed = 300000; 
-    double distance = 149600000;
-    double time;
+    double light_speed = 300000; //빛의 속도
+    double distance = 149600000; //태양과 거리
+    double time; //초기화 하지 않아도 됨
     
     time = distance / light_speed;
 
-    printf("빛의 속도는 %fkm/s \n", light_speed);
-    printf("태양과 지구와의 거리 %fkm \n", distance); 
-    printf("도달 시간은 %f초\n", time);
+    printf("빛의 속도: %fkm/s \n", light_speed);
+    printf("태양과 지구와의 거리: %fkm \n", distance); 
+    printf("도달 시간: %f초\n", time);
 
     return 0; 
 }
